a1a4bbb598d462c7c25fb09aab169e16b82aca8d
nXu <nxu@nxu.hu>
2016-10-29 18:20:25 +0200

    Layout fix

--- resources/views/pages/document-list.blade.php
+++ resources/views/pages/document-list.blade.php
@@ -12,14 +12,14 @@
                 </ol>
             </div>
         </div>
-        <div class="row">
-            <div class="col-xs-12">
-                <h1>Dokumentumok</h1>
-            </div>
-        </div>
 
         <div id="document-list">
             @forelse($documents as $document)
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h1>Dokumentumok</h1>
+                    </div>
+                </div>
                 <div class="row file-row">
                     <div class="col-xs-12 col-sm-8">
                         <a href="{{ url($document->file_path) }}" target="_blank" class="text-center visible-xs">
