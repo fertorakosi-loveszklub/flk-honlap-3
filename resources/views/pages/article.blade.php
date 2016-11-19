fe238a913fd93ad56bffd14916bcdf98f278a4ab
nXu <nxu@nxu.hu>
2016-11-19 21:19:57 +0100

    Fix share bug

--- resources/views/pages/article.blade.php
+++ resources/views/pages/article.blade.php
@@ -33,7 +33,7 @@
                     </p>
 
                     <div class="social">
-                        <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="large" data-mobile-iframe="false">
+                        <div class="fb-share-button" data-layout="button" data-size="large" data-mobile-iframe="false">
                             <a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Megosztás</a>
                         </div>
                     </div>
