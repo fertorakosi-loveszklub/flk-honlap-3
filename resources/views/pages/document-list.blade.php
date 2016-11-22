8830a51d4ec0a9bbf8e4dcaa97c4879b0d923d21
Zsolt Fekete <nabunub@hotmail.com>
2016-11-22 15:29:32 +0100

    Update document-list.blade.php

--- resources/views/pages/document-list.blade.php
+++ resources/views/pages/document-list.blade.php
@@ -14,12 +14,13 @@
         </div>
 
         <div id="document-list">
-            @forelse($documents as $document)
-                <div class="row">
-                    <div class="col-xs-12">
-                        <h1>Dokumentumok</h1>
-                    </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <h1>Dokumentumok</h1>
                 </div>
+            </div>
+
+            @forelse($documents as $document)
                 <div class="row file-row">
                     <div class="col-xs-12 col-sm-8">
                         <a href="{{ url($document->file_path) }}" target="_blank" class="text-center visible-xs">
