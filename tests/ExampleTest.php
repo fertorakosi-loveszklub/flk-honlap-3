ae86ca043e30ba5d756ee23fea270fefdc3c85ee
Zsolt Fekete <nabunub@hotmail.com>
2018-10-18 11:01:51 +0200

    Yet another test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -15,4 +15,9 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+    
+    public function testAnotherExample()
+    {
+        $this->assertFalse(false);
+    }
 }
